9aeb1cefe469bc09a3b65f42d87bcf528f07ff7c
Michael Olp <michi@quixotic-project.de>
2013-04-13 17:44:30 +0200

    added local jquery prepare controller for REST calls

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,24 @@
 /* Controllers */
 
 angular.module('filestop.controllers', []).
-  controller('homeCtrl', [function() {
+    controller('homeCtrl', ["$scope", "$location", "$http", function ($scope, $location, $http) {
+        $scope.newFilestop = function() {
+            console.log('creating new filestop');
 
-  }])
-  .controller('newFilestopCtrl', [function() {
+            $http({method: 'POST', url: '/filestops'}).
+                success(function(data, status, headers, config) {
+                    console.log('redirecting to the new filestop');
+                    alert(data);
+                    $location.path('/filestop/' + '4711');
+                }).
+                error(function(data, status, headers, config) {
+                    console.log('error while creating filestop');
+                    alert(status + data);
+                });
 
-  }]);
+
+        }
+    }])
+    .controller('filestopCtrl', [function () {
+
+    }]);
