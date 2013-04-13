12a89edc9ca9db6a3b71f7f9e1baafd12cefcb32
Michael Olp <michi@quixotic-project.de>
2013-04-13 18:58:53 +0200

    added a list of filestops to the home screen

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,7 @@
 angular.module('filestop', ['filestop.filters', 'filestop.services', 'filestop.directives', 'filestop.controllers', 'ui']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'homeCtrl'});
-    $routeProvider.when('/filestop', {templateUrl: 'partials/filestop.html', controller: 'newFilestopCtrl'});
+    $routeProvider.when('/filestop/:id', {templateUrl: 'partials/filestop.html', controller: 'filestopCtrl'});
     $routeProvider.otherwise({redirectTo: '/home'});
   }]);
 
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,22 +4,42 @@
 
 angular.module('filestop.controllers', []).
     controller('homeCtrl', ["$scope", "$location", "$http", function ($scope, $location, $http) {
-        $scope.newFilestop = function() {
+
+        $scope.filestops = {};
+
+        $scope.readRecentFilestops = function () {
+            console.log('reading recent filestops');
+            $http({method: 'GET', url: '/filestops'})
+                .success(function (data, status, headers, config) {
+                    $scope.filestops = data;
+                }).error(
+                function (data, status, headers, config) {
+                    console.log('error while reading current filestops');
+                    alert(status + data);
+                });
+        };
+
+        $scope.newFilestop = function () {
             console.log('creating new filestop');
 
             $http({method: 'POST', url: '/filestops'}).
-                success(function(data, status, headers, config) {
-                    console.log('redirecting to the new filestop');
+                success(function (data, status, headers, config) {
+                    console.log('redirecting to the new filestop with id ' + data.id);
                     $location.path('/filestop/' + data.id);
                 }).
-                error(function(data, status, headers, config) {
+                error(function (data, status, headers, config) {
                     console.log('error while creating filestop');
                     alert(status + data);
                 });
+        };
 
+        $scope.readRecentFilestops();
 
-        }
     }])
-    .controller('filestopCtrl', [function () {
+    .controller('filestopCtrl', ["$scope", "$routeParams", "$location", "$http", function ($scope, $routeParams, $location, $http) {
 
+
+        $scope.getId = function () {
+            return $routeParams.id;
+        }
     }]);
