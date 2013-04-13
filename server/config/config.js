97675d2c7e83d546e9f815ca97b7d0488734e856
Xemle <xemle@phtagr.org>
2013-04-13 17:50:30 +0200

    Add production config

--- server/config/config.js
+++ server/config/config.js
@@ -2,5 +2,9 @@ module.exports = {
     development: {
         db: "mongodb://localhost/filestoredbdev",
         port: 8000
+    },
+    production: {
+        db: "mongodb://localhost/filestoredb",
+        port: 8080
     }
 }
