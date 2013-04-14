99aa55aef83d34ea09af50612b2676c288810711
Michael Olp <michi@quixotic-project.de>
2013-04-14 13:56:09 +0200

    use pretty dates

--- app/js/filters.js
+++ app/js/filters.js
@@ -3,8 +3,13 @@
 /* Filters */
 
 angular.module('filestop.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
+    filter('interpolate', ['version', function (version) {
+        return function (text) {
+            return String(text).replace(/\%VERSION\%/mg, version);
+        }
+    }])
+    .filter('prettyDate', ['date', function (date) {
+        return function (date) {
+            return $scope.prettyDate(date);
+        }
+    }]);
