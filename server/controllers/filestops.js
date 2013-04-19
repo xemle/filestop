938b46e35a085ab979984ccb4652257c3b0ae7b7
Xemle <xemle@phtagr.org>
2013-04-19 20:35:43 +0200

    Fix validation in filestop with findOne() and save() call

--- server/controllers/filestops.js
+++ server/controllers/filestops.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose'),
     File = mongoose.model('File'),
     Error = require ("errno-codes"),
     Filestop = mongoose.model('Filestop');
-;
 
 module.exports = function (config) {
     var exports = {};
@@ -24,25 +23,25 @@ module.exports = function (config) {
     exports.update = function(req, res, next) {
         var cid = req.params.cid;
 
-        req.body.updated = new Date;
-
-        // find and update does not call custom validators right now
+        // findAndUpdate does not call custom validators right now
         // see this bug report: https://github.com/LearnBoost/mongoose/issues/860
-        // this is so sad..
-        Filestop.findOneAndUpdate ({cid: cid}, {$set: req.body}, function (err, filestop) {
-            if (err) {
-                console.log("Error updating Filestop with cid " + cid + ": " + err);
-                res.send({success: false, errors: err});
-                return;
-            }
-
-            if (filestop) {
-                console.log("Updated Filestop with cid " + cid);
-                res.send({success: 'OK', cid: filestop.cid});
-            } else {
-                console.log("Error updating Filestop with cid " + cid + ": not found");
-                res.send({success: false, errors: "Filestop not found"});
-            }
+        // this is so sad... Use workaround with findOne with save
+        Filestop.findOne({cid: cid}, function(err, filestop) {
+           if (err) {
+               res.send(404);
+           } else {
+               filestop.name = req.body.name;
+               filestop.description = req.body.description;
+               filestop.updated = new Date();
+               filestop.save(function(err) {
+                  if (err) {
+                      res.send(400, err);
+                  } else {
+                      console.log("Updated Filestop with cid " + cid);
+                      res.send(200);
+                  }
+               });
+           }
         });
     };
     exports.delete = function(req, res, next) {
@@ -52,21 +51,20 @@ module.exports = function (config) {
         File.find({filestopCId: cid}, function (err, result) {
             if (err) {
                 console.log("Error querying for files of filestop with cid " + cid, err);
-                return res.send({success: false, errors: "Error querying for files of filestop with cid " + cid});
+                return res.send(404);
             }
             if (result) {
                 result.forEach (function (file) {
                     file.deleteFile(config, function(err) {
                         // ENOENT is "File does not exist"
                         if (err && err.errno != Error.ENOENT) {
-                            res.send({success: 'false', error: err});
+                            console.log("Could not delete file");
                             return;
                         }
-
                         file.remove();
                     });
-
                 });
+                res.send(404);
             }
         });
 
@@ -110,7 +108,7 @@ module.exports = function (config) {
         File.find({filestopCId: cid}, function (err, result) {
             if (!result) {
                 console.log("No files found for Filestop with cid " + cid);
-                res.send(null);
+                res.send(404);
             }
             res.send(result);
         });
