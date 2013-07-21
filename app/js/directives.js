0770309f465aff17c36c79c87b556215547d7389
Xemle <xemle@phtagr.org>
2013-07-21 22:06:47 +0200

    Add 'Download All' button to bottom, too

--- app/js/directives.js
+++ app/js/directives.js
@@ -11,8 +11,8 @@ angular.module('filestop.directives', []).
   }])
     .directive('editable', function() {
         var inputTemplate = '<form class="form-inline" ng-show="isEditMode"><input type="text" ng-dblclick="switchToPreview()" ng-model="text"></input><button type="submit" class="btn" ng-click="switchToPreview()">Save</button></form>';
-        var showTemplate = '<span class="editable" ng-hide="isEditMode || isEmpty()" ng-dblclick="isEditMode = !isEditMode" >{{text}}</span>';
-        var emptyTemplate = '<span class="editable editable-empty" ng-show="isEmpty()" ng-dblclick="isEditMode = !isEditMode">Edit Me</span>';
+        var showTemplate = '<span class="editable" ng-hide="isEditMode || isEmpty()" ng-click="isEditMode = !isEditMode" >{{text}}</span>';
+        var emptyTemplate = '<span class="editable editable-empty" ng-show="isEmpty()" ng-click="isEditMode = !isEditMode">Click to change title</span>';
         return {
             scope: {
                 text: '=editable',
@@ -34,8 +34,8 @@ angular.module('filestop.directives', []).
         }
     }).directive('textable', function() {
         var inputTemplate = '<form ng-show="isEditMode"><textarea  type="text" ng-dblclick="switchToPreview()" ng-model="text" rows="5"></textarea><button type="submit" class="btn" ng-click="switchToPreview()">Save</button></form>';
-        var showTemplate = '<span class="editable" ng-hide="isEditMode || isEmpty()" ng-dblclick="isEditMode = !isEditMode" >{{text}}</span>';
-        var emptyTemplate = '<span class="editable editable-empty" ng-show="isEmpty()" ng-dblclick="isEditMode = !isEditMode">Edit Me</span>';
+        var showTemplate = '<span class="editable" ng-hide="isEditMode || isEmpty()" ng-click="isEditMode = !isEditMode" >{{text}}</span>';
+        var emptyTemplate = '<span class="editable editable-empty" ng-show="isEmpty()" ng-click="isEditMode = !isEditMode">Click to add a description</span>';
         return {
             scope: {
                 text: '=textable',
